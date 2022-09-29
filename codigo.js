fc91119294b425c83898c766eec9f06b3a7c207a
Camila Denise Goncalves <86990957+CamilaDen@users.noreply.github.com>
2022-09-28 23:07:04 -0300

    agregado de practica con js, iconos  y detalles

---
+++ codigo.js
@@ -0,0 +1,8 @@
+<script>
+    function saludar(){
+        var nombre = document.getElementById("txtNombre".value);
+        var mail = document.getElementById("txtMail".value);
+        console.log("Gracias " + nombre);
+        
+    }
+</script>
