e32af108fec85f947fbf8b44d3ac80a26484dbe4
Camila Denise Goncalves <86990957+CamilaDen@users.noreply.github.com>
2022-10-02 22:54:44 -0300

    Agregado de algunos links

--- codigo.js
+++ codigo.js
@@ -3,6 +3,5 @@
         var nombre = document.getElementById("txtNombre".value);
         var mail = document.getElementById("txtMail".value);
         console.log("Gracias " + nombre);
-        
     }
 </script>
